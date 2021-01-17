b69e03b1d242a861a87704615214df2a9e69d46e
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-16 19:57:47 -0500

    commit tree bc8e3f25a3bc9f5794b4e6aa859b00b5e165d2f9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    
+  end
+
+  def create
+    # byebug
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+ 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  # resources :posts, only: [:index]
+  # resources :posts, only: [:index, :new]
+  # resources :posts, only: [:index, :new, :create]
+
+  get '/posts', to: 'posts#index'
+
+  get '/posts/new', to: 'posts#new'
+
+  post '/posts', to: 'posts#create'
+
+
 end
